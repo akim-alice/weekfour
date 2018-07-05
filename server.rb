168991e08f14821a014df5e7e54659c90eba7548
alphonceakim <aliciakheem@gmail.com>
2018-07-05 09:06:11 +0300

    interacting with money app on the brows

--- server.rb
+++ server.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+ require_relative 'account.rb'
+
 
 get '/' do
  'Hello World!'
@@ -6,10 +8,6 @@ end
 
 get '/alice' do
  'Hello World!'
-end
-
-get '/:name' do
- "Hello #{params[:name]}"
 end
 
 get '/' do
@@ -20,6 +18,28 @@ get '/name' do
  'matched name'
 end
 
-get '/:age' do
- 'matched age'
+get '/deposit/:amount' do
+	"you have deposited #{params[:amount]}"
 end
+
+
+ get '/deposit' do
+ 	acc = Account.new
+ 	amount = params[ :amount].to_i
+ 	acc.deposit (amount)
+"your new balance is #{acc.balance}"
+end
+
+get "/withdraw" do
+   erb :withdraw
+end
+
+post "/withdraw" do
+	acc = Account.new
+	amount= params[:amount].to_i
+	agent_number = params[:agent_number].to_i
+	pin = params[:pin].to_i
+	acc.withdraw(amount, agent_number, pin)
+	"you have withdrawn #{amount} balance is #{acc.balance}"
+end
+
